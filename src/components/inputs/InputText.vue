20cf0da393cd7a2741844271ff5f693da07b3a83
GFrancV <gfrancv@hotmail.com>
2023-02-10 11:52:05 +0000

    feat(input-components): Abstract the text inputs with error status.

---
+++ src/components/inputs/InputText.vue
@@ -0,0 +1,50 @@
+<template>
+	<label class="form-label" :for="`${name}Input`">{{ title }}</label>
+	<input
+		:type="type"
+		class="form-control"
+		:class="{ 'is-invalid': haveErrors !== null }"
+		:name="`${name}Input`"
+		:value="modelValue"
+		@input="$emit('update:modelValue', $event.target.value)"
+		required
+	/>
+	<div v-if="haveErrors !== null" class="invalid-feedback">{{ haveErrors }}</div>
+</template>
+
+<script>
+	export default {
+		name: "InputText",
+		props: {
+			title: {
+				type: String,
+				required: true,
+			},
+			name: {
+				type: String,
+				required: true,
+			},
+			type: {
+				type: String,
+				default: "text",
+			},
+			modelValue: {
+				type: String,
+				required: true,
+			},
+			errors: {
+				type: Object,
+				default: {},
+			},
+		},
+		emits: ["update:modelValue"],
+
+		computed: {
+			haveErrors() {
+				return this.errors[this.name] ? this.errors[this.name][0] : null;
+			},
+		},
+	};
+</script>
+
+<style></style>
